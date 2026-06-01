8b48245b1201dfa8897799ee178ca7a621942b02
Denis Rylikov <63297132+drylikov@users.noreply.github.com>
2026-06-01 15:53:15 +0300

    Go_search
    Search Godoc.org via the command-line.

---
+++ Readme.md
@@ -0,0 +1,61 @@
+# Go_search.
+
+ [Godoc.org](http://godoc.org) via the command-line.
+
+ ![godoc command-line search]
+
+## Installation
+
+```
+$ go get github.com/drylikov/Go_search
+```
+
+## Usage
+
+- Add an alias: `alias gos=Go_search`
+- Help text:
+    ```sh
+    $ Go_search --help
+    Usage:
+        Go_search <query>... [--top] [--count n] [--open]
+        Go_search -h | --help
+        Go_search --version
+
+      Options:
+        -n, --count n    number of results [default: 5]
+        -t, --top        top-level packages only
+        -o, --open       open godoc.org search results in default browser
+        -h, --help       output help information
+        -v, --version    output version
+    ```
+- Examples:
+    ```sh
+    $ Go_search UUID
+
+        github.com/pborman/uuid
+        godoc.org/pkg/github.com/pborman/uuid
+        The uuid package generates and inspects UUIDs.
+
+        github.com/satori/go.uuid
+        godoc.org/pkg/github.com/satori/go.uuid
+        Package uuid provides implementation of Universally Unique
+        Identifier (UUID).
+
+        github.com/nu7hatch/gouuid
+        godoc.org/pkg/github.com/nu7hatch/gouuid
+        This package provides immutable UUID structs and the
+        functions NewV3, NewV4, NewV5 and Parse() for generating
+        versions 3, 4 and 5 UUIDs as specified in RFC 4122.
+
+        github.com/twinj/uuid
+        godoc.org/pkg/github.com/twinj/uuid
+        This package provides RFC4122 and DCE 1.1 UUIDs.
+
+        github.com/docker/distribution/uuid
+        godoc.org/pkg/github.com/docker/distribution/uuid
+        Package uuid provides simple UUID generation.
+    ```
+    ```sh
+    $ Go_search UUID --open
+    # opens godoc.org search results in default browser
+    ```
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/mitchellh/go-wordwrap"
+	gopen "github.com/petermbenjamin/go-open"
+	)
+
+// Response represents the response to be returned
+type Response struct {
+	Results []struct {
+		Path     string
+		Synopsis string
+	}
+}
+
+// Version is the package version
+var Version = "0.0.3"
+
+// Usage is the package usage information
+const Usage = `
+  Usage:
+    go-search <query>... [--top] [--count n] [--open]
+    go-search -h | --help
+    go-search --version
+
+  Options:
+    -n, --count n    number of results [default: 5]
+    -t, --top        top-level packages only
+    -o, --open       open godoc.org search results in default browser
+    -h, --help       output help information
+    -v, --version    output version
+
+`
+
+func main() {
+	args, err := docopt.Parse(Usage, nil, true, Version, false)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+	n, err := strconv.ParseInt(args["--count"].(string), 10, 32)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+	query := strings.Join(args["<query>"].([]string), " ")
+	top := args["--top"].(bool)
+
+	res, err := http.Get("https://api.godoc.org/search?q=" + url.QueryEscape(query))
+	if err != nil {
+		log.Fatalf("request failed: %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("request error: %s", http.StatusText(res.StatusCode))
+	}
+
+	var body Response
+	err = json.NewDecoder(res.Body).Decode(&body)
+	if err != nil {
+		log.Fatalf("error parsing response: %s", err)
+	}
+
+	if open := args["--open"].(bool); open {
+		gopen.Open("https://godoc.org/?q=" + url.QueryEscape(query))
+		os.Exit(0)
+	}
+
+	body.Results = body.Results[:n]
+
+	println()
+	for _, pkg := range body.Results {
+		if top && subpackage(pkg.Path) {
+			continue
+		}
+
+		fmt.Printf("  \033[1m%s\033[m\n", pkg.Path)
+		fmt.Printf("  godoc.org/pkg/%s\n", pkg.Path)
+		fmt.Printf("  %s\n", description(pkg.Synopsis))
+		fmt.Printf("\n")
+	}
+	println()
+}
+
+func subpackage(s string) bool {
+	switch {
+	case strings.HasPrefix(s, "github.com"):
+		return strings.Count(s, "/") > 3
+	default:
+		return false
+	}
+}
+
+func description(s string) string {
+	if s == "" {
+		return "no description"
+	}
+
+	s = wordwrap.WrapString(s, 60)
+	s = strings.Replace(s, "\n", "\n  ", -1)
+	return s
+}
